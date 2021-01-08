2d8386babfecf8acbd80d903d80c59fc410d8558
yu-miyano <nekokey38@gmail.com>
2021-01-08 11:42:41 +0900

    ActiveStorage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,11 @@ class ItemsController < ApplicationController
   def index
     @users = User.all
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :info, :price, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :image).merge(user_id: current_user.id)
+  end
+
 end
