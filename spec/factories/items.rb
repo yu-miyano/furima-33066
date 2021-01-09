36cac59cb60c8aadae4bfe6353e2a40398c0d64d
yu-miyano <nekokey38@gmail.com>
2021-01-09 20:30:17 +0900

    コード修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,12 +3,12 @@ FactoryBot.define do
 
     name                   {'hoge'}
     info                   {'hoge'}
-    price                  {'300'}
-    category_id            {'2'}
-    sales_status_id        {'2'}
-    shipping_fee_status_id {'2'}
-    prefecture_id          {'2'}
-    scheduled_delivery_id  {'2'}
+    price                  {300}
+    category_id            {2}
+    sales_status_id        {2}
+    shipping_fee_status_id {2}
+    prefecture_id          {2}
+    scheduled_delivery_id  {2}
     association :user
 
     after(:build) do |item|
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -33,31 +33,31 @@ RSpec.describe Item, type: :model do
       end
 
       it "カテゴリーの情報がないと出品できない" do
-        @item.category_id = "1"
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
 
       it "商品の状態の情報がないと出品出来ない" do
-        @item.sales_status_id = "1"
+        @item.sales_status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Sales status must be other than 1")
       end
 
       it "配送料の負担の情報がないと出品出来ない" do
-        @item.shipping_fee_status_id = "1"
+        @item.shipping_fee_status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping fee status must be other than 1")
       end
 
       it "発送元の地域の情報がないと出品出来ない" do
-        @item.prefecture_id = "1"
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
 
       it "発送までの日数の情報がないと出品出来ない" do
-        @item.scheduled_delivery_id = "1"
+        @item.scheduled_delivery_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Scheduled delivery must be other than 1")
       end
@@ -69,13 +69,13 @@ RSpec.describe Item, type: :model do
       end
 
       it "価格の範囲が300未満では出品出来ない" do
-        @item.price = "299"
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not included in the list")
       end
 
       it "価格の範囲が10000000以上では出品出来ない" do
-        @item.price = "10000000"
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not included in the list")
       end
