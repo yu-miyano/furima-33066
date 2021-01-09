4d5f1e3b94657cc91cd707daf155794beb71ac63
yu-miyano <nekokey38@gmail.com>
2021-01-09 19:57:03 +0900

    単体テストコード記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+
+    name                   {'hoge'}
+    info                   {'hoge'}
+    price                  {'300'}
+    category_id            {'2'}
+    sales_status_id        {'2'}
+    shipping_fee_status_id {'2'}
+    prefecture_id          {'2'}
+    scheduled_delivery_id  {'2'}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('spec/fixtures/test_image.jpg'), filename: 'test_image.jpg')
+    end
   end
 end
