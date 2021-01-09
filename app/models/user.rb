27017be850f1232f8855b1e846a9eb36a92aefbe
yu-miyano <nekokey38@gmail.com>
2021-01-09 20:50:52 +0900

    コード修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_many :intems
-  
+  has_many :items
+
   validates :nickname, presence: true
   validates :email, uniqueness: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
