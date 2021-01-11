f4b1a4880ece2a3cd7af52a94a9584d25a9dd6ac
yu-miyano <nekokey38@gmail.com>
2021-01-11 11:53:59 +0900

    before_action追加２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update]
+
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -19,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -43,9 +42,12 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :info, :price, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :image).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item =Item.find(params[:id])
+  end
+
   def move_to_index
-    item = Item.find(params[:id])
-    if item.user_id != current_user.id
+    if @item.user_id != current_user.id
       redirect_to action: :index
     end
   end
